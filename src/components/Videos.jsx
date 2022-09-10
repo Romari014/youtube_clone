d574d53ea8c75d071bf9a12714ed9d97faf9f361
Roman <roma00062@gmail.com>
2022-09-10 12:30:16 +0200

    Added data for Video component and created video box

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Stack, Box } from '@mui/material';
 
-const Videos = () => {
-  return (
-    <div>Videos</div>
-  )
+const Videos = ({ videos }) => {
+    console.log(videos);
+
+    return (
+        <Stack direction='row' flexWrap='wrap'
+            justifyContent='start' gap={2}>
+            {videos.map((item, index) => (
+                <Box key={index}>
+                    {item.id.videoId && <VideoCart video={item} />}
+                    {item.id.channelId && <ChannelCart channelDetail={item} />}
+                </Box>
+            ))}
+        </Stack>
+    )
 }
 
 export default Videos
